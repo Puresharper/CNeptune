dc3ed5fc72eed3dea918ed953bd67aee63bd5284
Jens <jho1965us@gmail.com>
2018-04-26 17:09:14 +0200

    Fixed unclear wording in comment

--- CNeptune/CNeptuneBase/NeptuneAttribute.cs
+++ CNeptune/CNeptuneBase/NeptuneAttribute.cs
@@ -27,7 +27,7 @@ namespace CNeptune
         /// </list>
         /// </summary>
         /// <remarks>
-        /// Come in handy where  generic as CNeptune has a bug generating code for them
+        /// Come in handy for generic as CNeptune has a bug generating code for them
         /// </remarks>
         /// <param name="managed"></param>
         public NeptuneAttribute(bool managed)
