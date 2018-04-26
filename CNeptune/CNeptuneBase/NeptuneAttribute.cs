295da3c8fa022d412230523ccd6ae0090c9ad34c
Jens <jho1965us@gmail.com>
2018-04-26 16:42:03 +0200

    Previous commit missing NeptuneAttribute class

---
+++ CNeptune/CNeptuneBase/NeptuneAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CNeptune
+{
+    [AttributeUsage(AttributeTargets.Assembly|AttributeTargets.Class|AttributeTargets.Struct|AttributeTargets.Property|AttributeTargets.Method|AttributeTargets.Constructor, Inherited = false)]
+    public class NeptuneAttribute : Attribute
+    {
+        public bool Managed { get; }
+
+        /// <summary>
+        /// Control if members of classes should be manged by Neptune.
+        /// NOT inherented from base but from outer scope (like generic parameters).
+        /// Usage:
+        /// <list type="table">
+        /// <item>
+        ///   <term>Assembly</term>
+        ///   <description>Default on for all classes, can be turned off </description>
+        /// </item>
+        /// <item>
+        ///   <term>class, struct</term>
+        ///   <description>Change default setting for members</description>
+        /// </item>
+        /// <item>
+        ///   <term>Property, method</term>
+        ///   <description>Change setting for specific member</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <remarks>
+        /// Come in handy where  generic as CNeptune has a bug generating code for them
+        /// </remarks>
+        /// <param name="managed"></param>
+        public NeptuneAttribute(bool managed)
+        {
+            Managed = managed;
+        }
+    }
+}
